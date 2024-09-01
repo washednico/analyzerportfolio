039965463f41596d6a8120e3f1bbf356cd4a4bb1
Nicola Fochi <81258087+washednico@users.noreply.github.com>
2024-09-01 14:47:17 +0200

    Delete example.py

--- example.py
+++
@@ -1,6 +0,0 @@
-import yfinance as yf
-
-exchange_rate_data = yf.download("EURUSD=X", period='1d', start ="2024-01-01", end=None)
-last_value = exchange_rate_data['Close'].iloc[-1]
-print(last_value)
-
