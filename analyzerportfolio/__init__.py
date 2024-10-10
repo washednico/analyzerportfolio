66817216e3266675713dc7658e240bc24ec120c1
Nicola Fochi <81258087+washednico@users.noreply.github.com>
2024-10-10 15:58:01 +0200

    Update __init__.py

--- analyzerportfolio/__init__.py
+++ analyzerportfolio/__init__.py
@@ -2,29 +2,31 @@ from .utils import get_currency
 from .utils import get_exchange_rate
 from .utils import get_stock_info
 from .utils import get_current_rate
+from .utils import download_data
+from .utils import create_portfolio
+from .utils import read_portfolio_composition
 
-from .metrics import calculate_beta_and_alpha
-from .metrics import calculate_sharpe_ratio
-from .metrics import calculate_sortino_ratio
-from .metrics import calculate_var
-from .metrics import download_data
-from .metrics import calculate_portfolio_scenarios
-from .metrics import calculate_dividend_yield
-from .metrics import calculate_max_drawdown
-from .metrics import calculate_analyst_suggestion
-from .metrics import calculate_portfolio_metrics
+from .metrics import c_total_return
+from .metrics import c_volatility
+from .metrics import c_beta
+from .metrics import c_sharpe
+from .metrics import c_sortino
+from .metrics import c_analyst_scenarios
+from .metrics import c_analyst_score
+from .metrics import c_dividend_yield
+from .metrics import c_VaR
+from .metrics import c_max_drawdown
+from .metrics import c_info_ratio
 
-from .graphics import compare_portfolio_to_market
-from .graphics import simulate_dca
+from .graphics import portfolio_value
 from .graphics import garch
 from .graphics import montecarlo
+from .graphics import drawdown
 from .graphics import heatmap
+from .graphics import distribution_return
+from .graphics import simulate_dca
 from .graphics import probability_cone
-from .graphics import drawdown_plot
-from .graphics import plot_distribution_returns
 
-from .ai import newsletter_report
-from .ai import get_suggestion
 from .ai import monitor_news
 
-from .optimization import markowitz_optimization
+from .optimization import optimize
